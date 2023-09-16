da1f451610f9289fb36913c05da71c863ebf76da
fantasticlife <michael@smethur.st:wq>
2023-09-16 15:34:18 +0100

    basic parliament period controller

---
+++ app/controllers/parliament_period_controller.rb
@@ -0,0 +1,14 @@
+class ParliamentPeriodController < ApplicationController
+  
+  def index
+    @page_title = 'Parliament periods'
+    @parliament_periods = ParliamentPeriod.all.order( 'start_on' )
+  end
+  
+  def show
+    parliament_period = params[:parliament_period]
+    @parliament_period = ParliamentPeriod.find( parliament_period )
+    @page_title = "#{@parliament_period.number.ordinalize} Parliament of the United Kingdom"
+    @sessions = @parliament_period.sessions
+  end
+end
