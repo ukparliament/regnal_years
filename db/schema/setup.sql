95f13fc783707f98faf64380364e48abb765233b
fantasticlife <michael@smethur.st:wq>
2023-09-16 16:07:12 +0100

    new tables for parliaments and sessions

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,3 +1,5 @@
+drop table if exists sessions;
+drop table if exists parliament_periods;
 drop table if exists regnal_years;
 drop table if exists reigns;
 drop table if exists monarchs;
@@ -30,5 +32,25 @@ create table regnal_years (
 	end_on date not null,
 	reign_id int not null,
 	constraint fk_reign foreign key (reign_id) references reigns(id),
+	primary key (id)
+);
+
+create table parliament_periods (
+	id int not null,
+	number int not null,
+	start_on date not null,
+	end_on date,
+	wikidata_id varchar(20) not null,
+	primary key (id)
+);
+
+create table sessions (
+	id serial not null,
+	number int not null,
+	start_on date not null,
+	end_on date,
+	citation varchar(20) not null,
+	parliament_period_id int not null,
+	constraint fk_parliament_period foreign key (parliament_period_id) references parliament_periods(id),
 	primary key (id)
 );
