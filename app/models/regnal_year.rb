28ad6424700316fcef5d82fa34f152f4b0109b10
fantasticlife <michael@smethur.st:wq>
2023-09-16 16:08:25 +0100

    added display dates method

--- app/models/regnal_year.rb
+++ app/models/regnal_year.rb
@@ -2,6 +2,12 @@ class RegnalYear < ApplicationRecord
   
   belongs_to :reign
   
+  def display_dates
+    display_dates = self.start_on.strftime( $DATE_DISPLAY_FORMAT ) + ' - '
+    display_dates += self.end_on.strftime( $DATE_DISPLAY_FORMAT ) if self.end_on != Date.today
+    display_dates
+  end
+  
   def display_label
     display_label = self.number.to_s
     display_label += ' ' + self.monarch_abbreviation
