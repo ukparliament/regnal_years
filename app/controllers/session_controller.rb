3e5bda1b4dd64228f5d13b5ac4ed588c78a4f6bd
fantasticlife <michael@smethur.st:wq>
2023-09-16 15:35:27 +0100

    basic session controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,28 @@
+class SessionController < ApplicationController
+  
+  def index
+    @page_title = 'Sessions'
+    @sessions = Session.find_by_sql(
+      "
+        SELECT s.*, pp.number AS parliament_period_number
+        FROM sessions s, parliament_periods pp
+        WHERE s.parliament_period_id = pp.id
+        ORDER BY start_on
+      "
+    )
+  end
+  
+  def show
+    session = params[:session]
+    @session = Session.find_by_sql(
+      "
+        SELECT s.*, pp.number AS parliament_period_number
+        FROM sessions s, parliament_periods pp
+        WHERE s.parliament_period_id = pp.id
+        AND s.id = #{session}
+        ORDER BY start_on
+      "
+    ).first
+    @page_title = "#{@session.number.ordinalize} session of the #{@session.parliament_period_number.ordinalize} Parliament of the United Kingdon"
+  end
+end
