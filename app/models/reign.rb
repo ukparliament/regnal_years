ecf8703cc03637b44a0b20d0d35eef4c89261e21
fantasticlife <michael@smethur.st:wq>
2023-09-16 19:32:07 +0100

    added regnal years method

--- app/models/reign.rb
+++ app/models/reign.rb
@@ -12,4 +12,17 @@ class Reign < ApplicationRecord
     display_title = self.monarch_title
     display_title += ' (' + self.display_dates + ')'
   end
+  
+  def regnal_years
+    RegnalYear.find_by_sql(
+      "
+        SELECT ry.*, m.abbreviation AS monarch_abbreviation
+        FROM regnal_years ry, reigns r, monarchs m
+        WHERE ry.reign_id = r.id
+        AND r.monarch_id = m.id
+        AND r.id = #{self.id}
+        ORDER BY start_on
+      "
+    )
+  end
 end
