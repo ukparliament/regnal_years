64259d9b22c3d1fb2b1cc374979fe8d7f6b1d08a
fantasticlife <michael@smethur.st:wq>
2023-09-18 11:00:42 +0100

    added sessions method

--- app/models/regnal_year.rb
+++ app/models/regnal_year.rb
@@ -13,4 +13,17 @@ class RegnalYear < ApplicationRecord
     display_label += ' ' + self.monarch_abbreviation
     display_label
   end
+  
+  def sessions
+    Session.find_by_sql(
+      "
+        SELECT s.*, pp.number AS parliament_period_number
+        FROM sessions s, parliament_periods pp, session_regnal_years sry
+        WHERE s.parliament_period_id = pp.id
+        AND s.id = sry.session_id
+        AND sry.regnal_year_id = #{self.id}
+        ORDER BY start_on
+      "
+    )
+  end
 end
