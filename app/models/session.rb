b693e4a56c28d1652afc58b070da6409b75d22fb
fantasticlife <michael@smethur.st:wq>
2023-09-18 11:11:05 +0100

    added methods to check if a session overlaps with a regnal year

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,4 +5,44 @@ class Session < ApplicationRecord
     display_dates += self.end_on.strftime( $DATE_DISPLAY_FORMAT ) if self.end_on
     display_dates
   end
+  
+  
+  # ## A method to determine whether a session overlaps a regnal year.
+  def overlaps_regnal_year?( regnal_year )
+    
+    # We sets overlaps regnal year to false.
+    overlaps_regnal_year = false
+    
+    # If the session has an end date and the regnal year has an end date ...
+    if self.end_on && regnal_year.end_on
+    
+      # If the session ends on or after after the regnal year starts and ends on or before the regnal year ends ...
+      if regnal_year.start_on <= self.end_on && regnal_year.end_on >= self.start_on
+        
+        # ... we set overlaps regal year to true.
+        overlaps_regnal_year = true
+      end
+      
+    # Otherwise, if neither the session nor the regnal year have an end data ...
+    elsif !self.end_on && !regnal_year.end_on
+      
+      # ... we must be in the latest session and the latest regnal year of the current reign, so we set overlaps regal year to true.
+      overlaps_regnal_year = true
+    end
+    overlaps_regnal_year
+  end
+  
+  def regnal_years
+    RegnalYear.find_by_sql(
+      "
+        SELECT ry.*, m.abbreviation AS monarch_abbreviation
+        FROM regnal_years ry, reigns r, monarchs m, session_regnal_years sry
+        WHERE ry.reign_id = r.id
+        AND r.monarch_id = m.id
+        AND ry.id = sry.regnal_year_id
+        AND sry.session_id = #{self.id}
+        ORDER BY start_on
+      "
+    )
+  end
 end
