23b05cc8f7a4bc871202976925757a068e3267b9
fantasticlife <michael@smethur.st:wq>
2023-09-21 09:31:06 +0100

    basic kingdom model

---
+++ app/models/kingdom.rb
@@ -0,0 +1,23 @@
+class Kingdom < ApplicationRecord
+  
+  def display_dates
+    display_dates = ''
+    display_dates = self.start_on.strftime( $DATE_DISPLAY_FORMAT ) if self.start_on
+    display_dates += ' - '
+    display_dates += self.end_on.strftime( $DATE_DISPLAY_FORMAT ) if self.end_on
+    display_dates
+  end
+  
+  def reigns
+    Reign.find_by_sql( 
+      "
+        SELECT r.*, k.name AS kingdom_name, m.title AS monarch_title
+        FROM REIGNS r, monarchs m, kingdoms k
+        WHERE r.monarch_id = m.id
+        AND r.kingdom_id = k.id
+        AND k.id = #{self.id}
+        ORDER BY r.start_on
+      "
+    )
+  end
+end
