7f6f3a4b059f33e41d99c601e0136b7125183e96
fantasticlife <michael@smethur.st>
2025-08-19 08:27:21 +0100

    added design system crumb and site title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  include LibraryDesign::Crumbs
+  
+  $SITE_TITLE = 'UK Parliament Session Citations'
   
   $DATE_DISPLAY_FORMAT = '%-d %B %Y'
 end
